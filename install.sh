7257ee3b622421d3753c3bc3bae6e88f6b9c6abc
Arthur <Sir*kitten566537>
2021-04-20 20:00:47 -0400

    Uploaded Configs

---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo pacman -Syyyu --needed polkit-gnome base-devel git picom nitrogen papirus-icon-theme rofi && cpr -rfv .config/ ~/ && git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si && paru -S pamac-aur nerd-fonts-jetbrains-mono matcha-gtk-theme
